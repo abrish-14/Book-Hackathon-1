a437cfe3d47e57bbcb4a609fd5459b7eb61ae5d8
Abrish Rathoor <159419534+abrish-14@users.noreply.github.com>
2026-01-08 06:31:26 +0500

    Update docusaurus.config.ts

--- my-website/website/docusaurus.config.ts
+++ my-website/website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-ai-spec-driven-book.example.com',
+  url: 'https://book-hackathon-1-5bhd.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
